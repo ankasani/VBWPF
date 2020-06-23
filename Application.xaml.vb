283cc27a6b898d6edacf609b7ada95c892ae86bf
Kasani Anvesh <ankasani@microsoft.com>
2020-06-23 17:40:02 +0530

    VBWPF

---
+++ Application.xaml.vb
@@ -0,0 +1,15 @@
+﻿Imports System.Text
+Imports Microsoft.AppCenter
+Imports Microsoft.AppCenter.Analytics
+Imports Microsoft.AppCenter.Crashes
+Class Application
+    Inherits System.Windows.Application
+    Protected Overrides Sub OnStartup(ByVal e As StartupEventArgs)
+        MyBase.OnStartup(e)
+
+        AppCenter.LogLevel = LogLevel.Verbose
+        AppCenter.Start("b11df179-5b62-45a9-afc2-311e0ffed838", GetType(Analytics), GetType(Crashes))
+        AppCenter.SetUserId("mskasani@VBwpf")
+        Crashes.GetErrorAttachments = Function(ByVal report As ErrorReport) New ErrorAttachmentLog() {ErrorAttachmentLog.AttachmentWithText("<!DOCTYPE html><html><body><h2>HTML Links</h2><p><a href=""https://www.w3schools.com/html/"">VB Visit our HTML tutorial</a></p></body></html>", "data.html"), ErrorAttachmentLog.AttachmentWithBinary(Encoding.UTF8.GetBytes("VB Fake image"), "fake_image.jpeg", "image/jpeg")}
+    End Sub
+End Class
---
+++ MainWindow.xaml.vb
@@ -0,0 +1,34 @@
+﻿Imports System.IO
+Imports Microsoft.AppCenter.Analytics
+Imports Microsoft.AppCenter.Crashes
+
+
+Class MainWindow
+    Inherits Window
+
+    Public Sub New()
+        InitializeComponent()
+    End Sub
+    Private Sub Button_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+        Crashes.GenerateTestCrash()
+    End Sub
+
+    Private Sub Button_Click_1(ByVal sender As Object, ByVal e As RoutedEventArgs)
+        Analytics.TrackEvent("VB Time event", New Dictionary(Of String, String) From {
+            {"Time", DateTime.Now.ToString()}
+        })
+    End Sub
+
+    Private Sub Button_Click_2(ByVal sender As Object, ByVal e As RoutedEventArgs)
+        Try
+
+            Using reader As StreamReader = New StreamReader("not-there.txt")
+                reader.ReadToEnd()
+            End Using
+
+        Catch ex As FileNotFoundException
+            Crashes.TrackError(ex)
+        End Try
+    End Sub
+
+End Class
